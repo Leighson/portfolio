26c24472f63945bb0811b75545d0c048d6892ff0
Lee Santos <lee.m.santos@outlook.com>
2023-07-18 20:11:16 -0400

    Delete unused files, and modify dependencies.

--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    .forEach(tooltip => {
-      new bootstrap.Tooltip(tooltip)
-    });
-
-const scrollSpy = new bootstrap.ScrollSpy(document.body, {
-      target: '#scrollspy-target'
-    });
